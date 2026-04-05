d1c14694e002e25cdd84d2891f2f678d47411f2a
Mitsuki Ogasahara <negiga@gmail.com>
2026-04-05 14:27:16 +0900

    Initial Commit

---
+++ whiplash/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  whiplash
+//
+//  Created by Mitsuki Ogasahara on 2026/04/05.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ whiplash/whiplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  whiplashApp.swift
+//  whiplash
+//
+//  Created by Mitsuki Ogasahara on 2026/04/05.
+//
+
+import SwiftUI
+
+@main
+struct whiplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
